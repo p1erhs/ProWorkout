da0302471d6bcdd674fca7f67f43da6e68102f1b
p1erhs <pierisip@csd.auth.gr>
2021-04-29 19:26:22 +0300

    Changed plan xml activity

--- app/src/main/java/com/example/proworkout/MainActivity.java
+++ app/src/main/java/com/example/proworkout/MainActivity.java
@@ -5,15 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
+    /**
+     * Αυτή η μέθοδος μετακινεί τον χρήστη σε άλλο activity όταν πατάει το button.
+     * @param view
+     */
     public void handleStart(View view){
         Intent intent= new Intent(MainActivity.this,ChoosePlan.class);
         startActivity(intent);
